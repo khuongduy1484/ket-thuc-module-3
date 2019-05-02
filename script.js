6471ce0988a4a175e4e19e1acb3580450e8fef36
khuongduy1484 <khuongduy1484@gmail.com>
2019-05-02 17:55:48 +0700

    bai 2

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function tryRemoverFromArray(arr,a) {
     for (let  i = 0;i<arr.length;i++) {
         if (a==i) {
-            arr.splice(a,1)
+            arr.splice(a,1);
+        }else {
+            arr.splice(a,0);
         }
-    }document.write(arr);
+    };document.write(arr);
 }
 let b = [1,2,3];
 tryRemoverFromArray(b,1);
